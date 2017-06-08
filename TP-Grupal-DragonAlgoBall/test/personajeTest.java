45a535738138b0ef2503858b5fd1dcba508f8fc4
Metallikapo <nico_sallis@hotmail.com>
2017-06-08 00:21:27 -0300

    Pasando el test 1, y probando subir desde intelliJ

--- TP-Grupal-DragonAlgoBall/test/personajeTest.java
+++ TP-Grupal-DragonAlgoBall/test/personajeTest.java
@@ -1,62 +1,122 @@
+import TPGrupalDragonAlgoBall.src.modelo.personajes.Goku;
+import TPGrupalDragonAlgoBall.src.modelo.tablero.Tablero;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class personajeTest{
 
     @Test
-    public void test01verificarPersonajeEnCasilleroNuevo(){
+    public void test01moverYVerificarPersonajeEnCasilleroNuevo(){
 
-        Jugador jugador1 = new Jugador("juagdor1");
-        Jugador jugador2 = new Jugador("jugador2");
         Goku goku = new Goku();
         Tablero tablero = new Tablero(7); // define tablero de 7x7
 
-        Casillero casillero = tablero.obtenerCasillero(1,3);
-        jugador1.ubicarPersonaje(goku,casillero);
+        tablero.ubicarPersonaje(goku,0,0);
 
-        Casillero casilleroNuevo = tablero.obtenerCasillero(0,1);
-
-        assertTrue(goku.verificarPosicion(casilleroNuevo));
+        assertTrue(tablero.sePuedeMoverPersonaje(goku,0,2));
     }
 
     @Test
     public void test02verificarQueHayaUnSoloPersonajeEnUnCasillero(){
 
         Goku goku = new Goku();
+        Gohan gohan = new Gohan();
         Tablero tablero = new Tablero(7); // define tablero de 7x7
 
-        Casillero casillero = tablero.obtenerCasillero(1,3);
-        //jugador1.ubicarPersonaje(goku,casillero);
+        tablero.ubicarPersonaje(goku,0,0);
+        tablero.ubicarPersonaje(gohan,0,1);
 
-        Casillero casilleroNuevo = tablero.obtenerCasillero(0,1);
-
-        asserTrue(casilleroNuevo.estaDesocupado());
+        //exception??
+        assertFalse(tablero.sePuedeMoverPersonaje(goku,0,1));
     }
 
     @Test
     public void test03verificarQuePersonajeNoPasePorCasilleroQueTienePersonaje(){
 
         Goku goku = new Goku();
-        Cell cell = new Cell();
-        Pasos conjuntoCeldas = new Pasos();
-
+        Gohan gohan = new Gohan();
         Tablero tablero = new Tablero(7); // define tablero de 7x7
 
-        Casillero casilleroGoku = tablero.obtenerCasillero(1,3);
-        //jugador1.ubicarPersonaje(goku,casillero);
+        tablero.ubicarPersonaje(goku,0,0);
+        tablero.ubicarPersonaje(gohan,1,1);
 
-        Casillero casilleroCell = tablero.obtenerCasillero(0,1);
-        Casillero casilleroNuevoCell = tableroObtenerCasillero(2,2);
-        conjuntoCeldas.agregarPaso(casilleroCell);
-        conjuntoCeldas.agregarPaso(casilleroNuevoCell);
-
-        assertTrue(tablero.caminoDesocupado(conjuntoCeldas));
+        //exception
+        assertFalse(tablero.sePuedeMoverPersonaje(goku,2,2));
 
     }
 
     @Test
     public void test04verificarTransformacion(){
+
+        Goku goku = new Goku();
+        Tablero tablero = new Tablero(7);
+
+        tablero.ubicarPersonaje(goku,0,0);
+        goku.modificarKi(20);
+
+        assertTrue(goku.sePuedeTransformar());
+    }
+
+    @Test
+    public void test05verificarUbicacionDespuesDeLaTransformacion(){
+
+        Goku goku = new Goku();
+        Tablero tablero = new Tablero(7);
+
+        tablero.ubicarPersonaje(goku,0,0);
+        goku.modificarKi(20);
+
+        goku.transformar();
+
+        assertTrue(tablero.sePuedeMoverPersonaje(goku,3,3));
+    }
+
+    @Test
+    public void test06inicializarPartida(){
+
+        Jugador jugador1 = new Jugador("1");
+        Jugador jugador2 = new Jugador("2");
+        //Puede existir una interfaz equipo
+        EquipoZ equipo1 = new EquipoZ();
+        EquipoEnemigo equipo2 = new EquipoEnemigo();
+        Tablero tablero = new Tablero(7);
+
+        jugador1.asignarEquipo(equipo1);
+        jugador2.asignarEquipo(equipo2);
+
+        tablero.inicializarTablero(equipo1,equipo2);
+
+        assertTrue(tablero.esGuerreroZ(0,0));
+        assertTrue(tablero.esGuerreroZ(0,1));
+        assertTrue(tablero.esGuerreroZ(1,0));
+        assertTrue(tablero.esEnemigo(6,6));
+        assertTrue(tablero.esEnemigo(5,6));
+        assertTrue(tablero.esEnemigo(6,5));
+    }
+
+    @Test
+    public void test07verificarAtaqueBasicoSegunDistancia(){
+
+        //Puede existir una interfaz equipo
+        Tablero tablero = new Tablero(7);
+        Goku goku = new Goku();
+        Cell cell = new Cell();
+
+        tablero.ubicarPersonaje(goku,0,0);
+        tablero.ubicarPersonaje(cell,3,3);
+
+        int distancia = tablero.calcularDistancia(goku,cell);
+        int vidaCell = cell.getPuntosVida();
+        goku.ataqueBasico(cell,distancia);
+
+        assertFalse(cell.getPuntosVida()<vidaCell);
+
+        int vidaGoku = goku.getPuntosVida();
+        cell.ataqueBasico(goku,distancia);
+
+        assertTrue(goku.getPuntosVida()<vidaGoku);
 
     }
 }
