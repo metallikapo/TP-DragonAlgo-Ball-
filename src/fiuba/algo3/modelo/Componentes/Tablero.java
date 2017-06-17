9f8a309d1d842190d09340615523e3ded791ebaa
NoeliaNF <noe_nf90@yahoo.com.ar>
2017-06-17 15:04:17 -0300

    Actualizacion

--- src/fiuba/algo3/modelo/Componentes/Tablero.java
+++ src/fiuba/algo3/modelo/Componentes/Tablero.java
@@ -16,27 +16,29 @@ public class Tablero {
     	this.celdasOcupadas = new ArrayList<Celda>();	
     }
     
-    public void colocarCeldaEnTablero(Celda celda){
-    	if (this.celdaOcupada(celda)){
-    		throw new CeldaOcupadaException();
-    	}
-    	
-    	this.celdasOcupadas.add(celda);
-    }
     
     private boolean celdaOcupada(Celda unaCelda){
     	Iterator<Celda> iterador =this.celdasOcupadas.iterator();
     	boolean encontrado = false;
     	Celda celda;
     	
-    	while (iterador.hasNext() && ! encontrado)
+    	while (iterador.hasNext() && !encontrado)
     	{
     		celda = iterador.next();
-    		encontrado = ( celda.getCoordenada().getCoordenadaX() == unaCelda.getCoordenada().getCoordenadaX()
-    				&& celda.getCoordenada().getCoordenadaY() == unaCelda.getCoordenada().getCoordenadaY());
+    		encontrado = ( (celda.getCoordenada().getCoordenadaX()) == (unaCelda.getCoordenada().getCoordenadaX())
+    				&& (celda.getCoordenada().getCoordenadaY()) == (unaCelda.getCoordenada().getCoordenadaY()) );
+    	}
+
+    	return encontrado;
+    }
+    
+    
+    public void colocarCeldaEnTablero(Celda celda){
+    	if (this.celdaOcupada(celda)){
+    		throw new CeldaOcupadaException();
     	}
     	
-    	return encontrado;
+    	this.celdasOcupadas.add(celda);
     }
     
 }
