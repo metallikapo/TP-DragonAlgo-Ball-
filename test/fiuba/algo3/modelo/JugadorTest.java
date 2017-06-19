b0f39cbb85d96aaba41b29f44cc0041cf6e08efa
Metallikapo <nico_sallis@hotmail.com>
2017-06-19 00:20:41 -0300

    Primer boceto de Jugador y Partida

---
+++ test/fiuba/algo3/modelo/JugadorTest.java
@@ -0,0 +1,14 @@
+package fiuba.algo3.modelo;
+
+import fiuba.algo3.modelo.Juego.Jugador;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JugadorTest {
+    @Test
+    public void crearJugador(){
+        Jugador jugador = new Jugador("Juan");
+    }
+
+}
